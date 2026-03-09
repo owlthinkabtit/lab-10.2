91891b0646a40c9a87a78c3255152876eaaf695e
Meek Meek <haggins.jameka@gmail.com>
2026-03-09 13:46:25 -0400

    Finally getting back to working on this Lab, since I never started. Working through the debouncing portion of it, and just saving what I have currently. Just in case this is a working version, want to save, since I'm not too sure if I'm doing this correctly.

--- src/components/DebounceSearch.jsx
+++ src/components/DebounceSearch.jsx
@@ -8,7 +8,30 @@ function DebounceSearch() {
   const filteredFruits = FRUITS.filter((fruit) => fruit.toLowerCase().includes(debouncedSearch.toLowerCase()))
 
   return(
-    <>
-    </>
-  )
-}
+    <div style={{ padding: '20px', border: '1px solid #ccc'}}>
+      <h2>Fruit Finder</h2>
+      <input 
+        type="text"
+        placeholder="Search for a fruit...."
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+        style={{ padding: '8px', width: '250px'}}
+      />  
+
+      <p><i>Status: {searchTerm !== debouncedSearch ? "Typing..." : "Done!"}</i></p>
+      <h4>Results (Searching for: "{debouncedSearch}"):</h4>
+
+      <ul style={{ maxHeight: '200px', overflowY: 'auto'}}>
+        {filteredFruits.lenght > 0 ? (
+          filteredFruits.map((fruit, index) => (
+            <li key={index}>{fruit}</li>
+          ))
+        ) : (
+          <li>No fruits found!</li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default DebounceSearch;
